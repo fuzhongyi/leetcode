efec273088fc3a1e53a2b5131d93a0cbac8f4981
fuzhongyi <fuzhongyi@doctorwork.com>
2020-04-13 19:18:22 +0800

    feat: 反转字符串

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,40 +1,17 @@
 module.exports = {
-  title: "Hello VuePress",
+  title: "leetcode",
   description: "Just playing around",
-  plugins: ["@vuepress/active-header-links", "@vuepress/back-to-top"],
   themeConfig: {
     nav: [
-      { text: "Home", link: "/" }, // 根路径
-      { text: "Guide", link: "/guide/" },
-      { text: "External", link: "https://google.com" }, // 外部链接
-      // 显示下拉列表
+      { text: "主页", link: "/" },
+      { text: "GitHub", link: "https://github.com/fuzhongyi/leetcode" },
+    ],
+    sidebar: [
       {
-        text: "Languages",
-        items: [
-          { text: "Chinese", link: "/language/chinese" },
-          { text: "Japanese", link: "/language/japanese" }
-        ]
+        title: "递归",
+        collapsable: false,
+        children: ["/guide/recursion", "/guide/recursion/print-reverse.md"],
       },
-      // 下拉列表显示分组
-      {
-        text: "高级",
-        items: [
-          {
-            text: "算法",
-            items: [
-              { text: "冒泡", link: "/language/chinese" },
-              { text: "快速", link: "/language/japanese" }
-            ]
-          },
-          {
-            text: "设计模式",
-            items: [
-              { text: "工厂", link: "/language/chinese" },
-              { text: "单例", link: "/language/chinese" }
-            ]
-          }
-        ]
-      }
-    ]
-  }
+    ],
+  },
 };
---
+++ docs/guide/recursion/print-reverse.md
@@ -0,0 +1,43 @@
+# 反转字符串
+
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 `char[]` 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须[原地](https://baike.baidu.com/item/原地算法)**修改输入数组**、使用 `O(1)` 的额外空间解决这一问题。
+
+你可以假设数组中的所有字符都是 [ASCII](https://baike.baidu.com/item/ASCII) 码表中的可打印字符。
+
+示例 1：
+
+```bash
+输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+```
+
+示例 2：
+
+```bash
+输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+```
+
+```js
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    if(!s || s.length === 0) return s;
+    helper(s, 0, s.length - 1);
+    return s;
+};
+
+function helper(s, start, end) {
+    if(start >= end) return;
+    var temp = s[start];
+    s[start] = s[end];
+    s[end] = temp;
+    start++;
+    end--;
+    helper(s, start, end);
+}
+```
